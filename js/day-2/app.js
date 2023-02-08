77ecaf71b39c8d66a1a94fae7d216a940ab9c4e6
Preslav Getov <preslav.getov@yara.com>
2023-02-08 17:26:08 +0200

    basic styling on buttons done

--- js/day-2/app.js
+++ js/day-2/app.js
@@ -1,1 +1,18 @@
-console.log('hey')
+console.log('hey');
+function checkBattery(){
+
+}
+
+function add(){
+
+}
+
+function testButton(){
+    const firstButtonElement = document.querySelector('.single-item-number');
+    console.log(firstButtonElement)
+    firstButtonElement.addEventListener('click', ()=>{
+        console.log('hey i was clicked !');
+    })
+}
+
+testButton();
