e9d7309a6ea992f4a0a6b17dbff618f04d370a43
Preslav Getov <preslav.getov@yara.com>
2023-02-09 15:43:51 +0200

    still can not eval basic expressions

--- js/day-2/app.js
+++ js/day-2/app.js
@@ -1,11 +1,17 @@
 const re = /[\*\/\-\+]/;
+const reMultiple = /[\+\-\/\*]/g;
 
 const calculatorDisplayFieldElement = document.querySelector('.calculator-display');
+calculatorDisplayFieldElement.addEventListener('input', (e)=>{
+    console.log('hey')
+    if(e.currentTarget.value.includes('ERR')){
+        console.log("yes")
+        Array.from(document.querySelectorAll('.single-item-number')).forEach(btn=>btn.disabled = true);
+    }
+})
 calculatorDisplayFieldElement.value = '0';
 
 let lastOperationResultMemory = '';
-//this line tests that the initial display value can be removed
-calculatorDisplayFieldElement.value = '';
 
 const calculatorMainframe = document.getElementsByClassName('calculator-operations')[0];
 calculatorMainframe.addEventListener('click', (e) => {
@@ -13,19 +19,29 @@ calculatorMainframe.addEventListener('click', (e) => {
         utilizeCButton();
     } else if (e.target.innerText == 'AC') {
         utilizeACButton();
+    } else if (e.target.innerText == '=') {
+        console.log(utilizeEquals(calculatorDisplayFieldElement.value));
     } else {
-        //this is where we eval everything else
-        calculatorDisplayFieldElement.value += e.target.innerText;
+        if (calculatorDisplayFieldElement.value.length == 8) {
+            // calculatorDisplayFieldElement.value = calculatorDisplayFieldElement.value.slice(0, 8);
+            lastOperationResultMemory = calculatorDisplayFieldElement.value;
+            calculatorDisplayFieldElement.value = 'ERR';
+            console.log(calculatorDisplayFieldElement);
+        } else {
+            calculatorDisplayFieldElement.value += e.target.innerText;
+        }
     }
 
     console.log(`value is '${lastOperationResultMemory}'`);
-
-    //calculatorDisplayFieldElement.value += itemSelected.textContent.trim();
 });
 /** will test whether special operation symbols exist within a specified math expression/combination of digits. */
 function testForSpecifiedStringWithinUserInput(valueAsString, reg) {
-    //return valueAsString.match(reg);
     return reg.test(valueAsString);
+}
+/** will test for multiple special chars */
+function testForMultipleSpecialChars(valueAsString, reg) {
+    let match = valueAsString.match(reg);
+    return match;
 }
 
 /** will clear both display and internal memory. */
@@ -43,8 +59,12 @@ function utilizeCButton() {
         lastOperationResultMemory += currentRawResult;
     } else {
         currentRawResult = '0';
-        lastOperationResultMemory = '0';
+        lastOperationResultMemory = '';
     }
 
     calculatorDisplayFieldElement.value = currentRawResult;
+}
+
+function utilizeEquals(stringInput) {
+    return testForMultipleSpecialChars(stringInput, reMultiple);
 }
