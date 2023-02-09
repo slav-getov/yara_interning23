068be7eb7c3c91725096397acf5d54cc4d9ade6c
Preslav Getov <preslav.getov@yara.com>
2023-02-09 11:34:14 +0200

    c key func done

--- js/day-2/app.js
+++ js/day-2/app.js
@@ -1,3 +1,5 @@
+const re = /[\*\/\-\+]/;
+
 const calculatorDisplayFieldElement = document.querySelector('.calculator-display');
 calculatorDisplayFieldElement.value = '0';
 
@@ -6,22 +8,34 @@ let lastOperationResultMemory = '';
 calculatorDisplayFieldElement.value = '';
 
 //attach an event listener to all buttons
-Array.from(document.querySelectorAll('.single-item-number')).forEach(itemSelected => {
-    itemSelected.addEventListener('click', (e)=> {
+Array.from(document.querySelectorAll('.single-item-number')).forEach((itemSelected) => {
+    itemSelected.addEventListener('click', (e) => {
         //trim is necessary since format of prettier adds whitespace before and after
 
-        if(e.target.innerText == 'C'){
-            lastOperationResultMemory += calculatorDisplayFieldElement.value;
-            calculatorDisplayFieldElement.value = lastOperationResultMemory;
-        }else if(e.target.innerText == 'AC'){
+        if (e.target.innerText == 'C') {
+            let currentRawResult = calculatorDisplayFieldElement.value;
+
+            if (testForSpecifiedStringWithinUserInput(currentRawResult, re)) {
+                currentRawResult = currentRawResult.replace(re, '');
+            }else{
+                currentRawResult = '0'
+            }
+
+            lastOperationResultMemory += currentRawResult;
+            calculatorDisplayFieldElement.value = currentRawResult;
+        } else if (e.target.innerText == 'AC') {
             lastOperationResultMemory = '';
-            calculatorDisplayFieldElement.value = '0'
-        }else{
+            calculatorDisplayFieldElement.value = '0';
+        } else {
+            //this is where we eval everything else
             calculatorDisplayFieldElement.value += e.target.innerText;
         }
-       
-        console.log(`value is '${lastOperationResultMemory}'`)
-        //calculatorDisplayFieldElement.value += itemSelected.textContent.trim();
-    })
-})
 
+        console.log(`value is '${lastOperationResultMemory}'`);
+        //calculatorDisplayFieldElement.value += itemSelected.textContent.trim();
+    });
+});
+/** will test whether special operation symbols exist within a specified math expression/combination of digits */
+function testForSpecifiedStringWithinUserInput(valueAsString, reg) {
+    return reg.test(valueAsString);
+}
