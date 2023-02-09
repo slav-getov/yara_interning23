52c48421fc9e7310fdf4cda7dbc3a13669d40142
Preslav Getov <preslav.getov@yara.com>
2023-02-09 12:38:46 +0200

    major issue in C button

--- js/day-2/app.js
+++ js/day-2/app.js
@@ -8,32 +8,49 @@ let lastOperationResultMemory = '';
 calculatorDisplayFieldElement.value = '';
 
 const calculatorMainframe = document.getElementsByClassName('calculator-operations')[0];
-calculatorMainframe.addEventListener('click', (e)=>{
-    //trim is necessary since format of prettier adds whitespace before and after
+calculatorMainframe.addEventListener('click', (e) => {
+    if (e.target.innerText == 'C') {
+        utilizeCButton();
+    } else if (e.target.innerText == 'AC') {
+        utilizeACButton();
+    } else {
+        //this is where we eval everything else
+        calculatorDisplayFieldElement.value += e.target.innerText;
+    }
 
-        if (e.target.innerText == 'C') {
-            let currentRawResult = calculatorDisplayFieldElement.value;
-
-            if (testForSpecifiedStringWithinUserInput(currentRawResult, re)) {
-                currentRawResult = currentRawResult.replace(re, '');
-            }else{
-                currentRawResult = '0'
-            }
-
-            lastOperationResultMemory += currentRawResult;
-            calculatorDisplayFieldElement.value = currentRawResult;
-        } else if (e.target.innerText == 'AC') {
-            lastOperationResultMemory = '';
-            calculatorDisplayFieldElement.value = '0';
-        } else {
-            //this is where we eval everything else
-            calculatorDisplayFieldElement.value += e.target.innerText;
-        }
-
-        console.log(`value is '${lastOperationResultMemory}'`);
-        //calculatorDisplayFieldElement.value += itemSelected.textContent.trim();
-})
-/** will test whether special operation symbols exist within a specified math expression/combination of digits */
+    console.log(`value is '${lastOperationResultMemory}'`);
+    
+    //calculatorDisplayFieldElement.value += itemSelected.textContent.trim();
+});
+/** will test whether special operation symbols exist within a specified math expression/combination of digits. */
 function testForSpecifiedStringWithinUserInput(valueAsString, reg) {
-    return reg.test(valueAsString);
+    return valueAsString.match(reg);
+}
+
+
+/** will clear both display and internal memory. */
+function utilizeACButton() {
+    lastOperationResultMemory = '';
+    calculatorDisplayFieldElement.value = '0';
+}
+
+/** will clear display in steps. First clear will remove unfinished expressions. Second clear will zero-out. */
+function utilizeCButton() {
+    let currentRawResult = calculatorDisplayFieldElement.value;
+    console.log(testForSpecifiedStringWithinUserInput(currentRawResult, re) + 'these are the macthes')
+    console.log(lastOperationResultMemory);
+    if ([testForSpecifiedStringWithinUserInput(currentRawResult, re)].length == 1) {
+        console.log('executed and found');
+        currentRawResult = currentRawResult.replace(re, '');
+        lastOperationResultMemory += currentRawResult;
+        // fix testFor...>!!!!
+    } else{
+        console.log('executed and not found');
+        currentRawResult = '0';
+        lastOperationResultMemory = '0';
+    }
+
+    //lastOperationResultMemory += currentRawResult;
+    console.log('very end of func' + currentRawResult)
+    calculatorDisplayFieldElement.value = currentRawResult;
 }
