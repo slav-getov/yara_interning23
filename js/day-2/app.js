a1737e9296361d89e13294362871c714821fee36
Preslav Getov <preslav.getov@yara.com>
2023-02-09 16:22:59 +0200

    ac and c function

--- js/day-2/app.js
+++ js/day-2/app.js
@@ -2,14 +2,8 @@ const re = /[\*\/\-\+]/;
 const reMultiple = /[\+\-\/\*]/g;
 
 const calculatorDisplayFieldElement = document.querySelector('.calculator-display');
-calculatorDisplayFieldElement.addEventListener('input', (e)=>{
-    console.log('hey')
-    if(e.currentTarget.value.includes('ERR')){
-        console.log("yes")
-        Array.from(document.querySelectorAll('.single-item-number')).forEach(btn=>btn.disabled = true);
-    }
-})
-calculatorDisplayFieldElement.value = '0';
+// note to self - don't try to check for inpuit change when changing inpuit via js. wont work. you need custom events
+calculatorDisplayFieldElement.placeholder = '0';
 
 let lastOperationResultMemory = '';
 
@@ -23,10 +17,10 @@ calculatorMainframe.addEventListener('click', (e) => {
         console.log(utilizeEquals(calculatorDisplayFieldElement.value));
     } else {
         if (calculatorDisplayFieldElement.value.length == 8) {
-            // calculatorDisplayFieldElement.value = calculatorDisplayFieldElement.value.slice(0, 8);
+            
             lastOperationResultMemory = calculatorDisplayFieldElement.value;
             calculatorDisplayFieldElement.value = 'ERR';
-            console.log(calculatorDisplayFieldElement);
+            
         } else {
             calculatorDisplayFieldElement.value += e.target.innerText;
         }
@@ -47,7 +41,7 @@ function testForMultipleSpecialChars(valueAsString, reg) {
 /** will clear both display and internal memory. */
 function utilizeACButton() {
     lastOperationResultMemory = '';
-    calculatorDisplayFieldElement.value = '0';
+    calculatorDisplayFieldElement.value = '';
 }
 
 /** will clear display in steps. First clear will remove unfinished expressions. Second clear will zero-out. */
@@ -58,7 +52,7 @@ function utilizeCButton() {
         currentRawResult = currentRawResult.replace(re, '');
         lastOperationResultMemory += currentRawResult;
     } else {
-        currentRawResult = '0';
+        currentRawResult = '';
         lastOperationResultMemory = '';
     }
 
