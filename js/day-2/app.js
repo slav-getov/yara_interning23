742971d556bbf1637b26858d72a682d8b9d4897d
Preslav Getov <preslav.getov@yara.com>
2023-02-09 10:27:12 +0200

    basic C and AC buttons functionality done

--- js/day-2/app.js
+++ js/day-2/app.js
@@ -1,7 +1,7 @@
 const calculatorDisplayFieldElement = document.querySelector('.calculator-display');
 calculatorDisplayFieldElement.value = '0';
 
-
+let lastOperationResultMemory = '';
 //this line tests that the initial display value can be removed
 calculatorDisplayFieldElement.value = '';
 
@@ -9,9 +9,19 @@ calculatorDisplayFieldElement.value = '';
 Array.from(document.querySelectorAll('.single-item-number')).forEach(itemSelected => {
     itemSelected.addEventListener('click', (e)=> {
         //trim is necessary since format of prettier adds whitespace before and after
-        console.log(e.target.innerText)
-        
-        calculatorDisplayFieldElement.value += itemSelected.textContent.trim();
+
+        if(e.target.innerText == 'C'){
+            lastOperationResultMemory += calculatorDisplayFieldElement.value;
+            calculatorDisplayFieldElement.value = lastOperationResultMemory;
+        }else if(e.target.innerText == 'AC'){
+            lastOperationResultMemory = '';
+            calculatorDisplayFieldElement.value = '0'
+        }else{
+            calculatorDisplayFieldElement.value += e.target.innerText;
+        }
+       
+        console.log(`value is '${lastOperationResultMemory}'`)
+        //calculatorDisplayFieldElement.value += itemSelected.textContent.trim();
     })
 })
 
